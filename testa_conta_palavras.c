4adcca80fa535a02ac3f3a5ed1ef0dac477eba9d
joaocabeca <joaopedro.df12@gmail.com>
2025-01-13 10:12:42 -0300

    teste de ordenacao de palavras falhando

--- testa_conta_palavras.c
+++ testa_conta_palavras.c
@@ -17,5 +17,13 @@ int main() {
     assert(num_words == 7);
 
     printf("Teste de contagem de palavras no arquivo: OK\n");
+
+    // Testa a ordenação das palavras
+    printf(words);
+    sort_words(words, num_words);
+    assert(strcmp(words[0].word, "é") == 0); 
+    assert(strcmp(words[1].word, "este") == 0);
+    printf("Teste da ordenacao das palavras: OK\n");
+
     return 0;
 }
